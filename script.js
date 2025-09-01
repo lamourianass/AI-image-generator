db5de9bef9e96915775b3b3e340824ebba2a5989
Anass Lamouri <anasslamouri@icloud.com>
2025-09-01 16:44:16 +0100

    fetch() #js

--- script.js
+++ script.js
@@ -14,7 +14,35 @@ function generateImage() {
     return;
   }
   setLoadingState(true)
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + API_KEY);
+
+  const formData = new FormData()
+  formData.append('prompt', promptValue)
+  formData.append('style', styleValue)
+  formData.append('aspect_ratio', ratioValue)
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: formData,
+    redirect: 'follow'
+  };
+
+  fetch(API_URL, requestOptions)
+    .then(response => response.blob())
+    .then(blob => {
+      const imageURL = URL.createObjectURL(blob)
+      imageResultElement.src = imageURL
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('An error occurred while generating the image.')
+    })
+    .finally(() => {
+      setLoadingState(false)
+    })
 }
+
 
 function setLoadingState(isLoading) {
   if (isLoading) {
